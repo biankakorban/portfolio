ef3b96280795574810ef5e718d31c0eaf325e55c
Bianka Korban <bianka.korban@gmail.com>
2020-05-27 00:37:27 +1000

    Updates

---
+++ precache-manifest.38b5a35d808c8cc22746b6cf7defa579.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa23b1d559cc39c4b8f8d978b507a29",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d4ead09c5df06186073",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d75128812603b7eeebaf",
+    "url": "/portfolio/static/css/main.ff3843fd.chunk.css"
+  },
+  {
+    "revision": "3d4ead09c5df06186073",
+    "url": "/portfolio/static/js/2.a11f37a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.a11f37a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75128812603b7eeebaf",
+    "url": "/portfolio/static/js/main.959226d2.chunk.js"
+  },
+  {
+    "revision": "885124f2d59392ad52cc",
+    "url": "/portfolio/static/js/runtime-main.b8a79a24.js"
+  },
+  {
+    "revision": "fcb65360ac47691500013cdf378d7bdb",
+    "url": "/portfolio/static/media/github6.fcb65360.png"
+  },
+  {
+    "revision": "03c66be383360c4bba89db0551902463",
+    "url": "/portfolio/static/media/image-bg.03c66be3.jpg"
+  },
+  {
+    "revision": "5a054efcfc597ea9502b932a9d0300b5",
+    "url": "/portfolio/static/media/profile_image.5a054efc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/biankakorban-portfolio/precache-manifest.e0d39793349fb9fd84dc4bc91bef7908.js"
+  "/portfolio/precache-manifest.38b5a35d808c8cc22746b6cf7defa579.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/biankakorban-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
